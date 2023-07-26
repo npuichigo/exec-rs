661ac4716593b9f989ae7deecc7e2658a18df2ba
Yuchao Zhang <418121364@qq.com>
2023-07-26 22:05:40 +0800

    Add error support to into_awaitable

--- exec/src/consumers/into_awaitable.rs
+++ exec/src/consumers/into_awaitable.rs
@@ -1,9 +1,10 @@
-use exec_core::receiver::SetValue;
+use exec_core::receiver::{SetError, SetValue};
 use exec_core::{OperationState, Sender};
 use std::cell::UnsafeCell;
+use std::error::Error;
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::Arc;
+use std::rc::Rc;
 use std::task::{Context, Poll, Waker};
 
 #[allow(dead_code)]
@@ -20,7 +21,7 @@ struct SharedState<V, E> {
 }
 
 pub struct Receiver<V, E> {
-    state: Arc<UnsafeCell<SharedState<V, E>>>,
+    state: Rc<UnsafeCell<SharedState<V, E>>>,
 }
 
 impl<V, E> SetValue for Receiver<V, E> {
@@ -36,11 +37,24 @@ impl<V, E> SetValue for Receiver<V, E> {
     }
 }
 
+impl<V, E: Error> SetError for Receiver<V, E> {
+    type Error = E;
+
+    fn set_error(self, error: Self::Error) {
+        unsafe {
+            let _ = (*self.state.get()).result.insert(AwaitResult::Error(error));
+            if let Some(waker) = (*self.state.get()).waker.take() {
+                waker.wake();
+            }
+        }
+    }
+}
+
 pub struct SenderAwaitable<S, V, E>
 where
     S: Sender<Receiver<V, E>, Value = V, Error = E>,
 {
-    state: Arc<UnsafeCell<SharedState<V, E>>>,
+    state: Rc<UnsafeCell<SharedState<V, E>>>,
     operation: S::Operation,
 }
 
@@ -49,7 +63,7 @@ where
     S: Sender<Receiver<V, E>, Value = V, Error = E>,
 {
     pub(crate) fn new(sender: S) -> Self {
-        let state = Arc::new(UnsafeCell::new(SharedState {
+        let state = Rc::new(UnsafeCell::new(SharedState {
             result: None,
             waker: None,
         }));
@@ -93,7 +107,9 @@ where
 #[cfg(test)]
 mod tests {
     use crate::consumers::SenderAwaitable;
+    use crate::factories::just_error;
     use crate::{just, then};
+    use exec_test::errors::TestError;
     use futures::executor::block_on;
 
     #[test]
@@ -101,6 +117,15 @@ mod tests {
         block_on(async {
             let sender = just(1);
             let sender = then(sender, |x| x + 1);
+            let awaitable = SenderAwaitable::new(sender);
+            println!("{:?}", awaitable.await);
+        });
+    }
+
+    #[test]
+    fn test_awaitable_error() {
+        block_on(async {
+            let sender = just_error(TestError);
             let awaitable = SenderAwaitable::new(sender);
             println!("{:?}", awaitable.await);
         });
